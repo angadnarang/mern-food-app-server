41646761550a06eee1f7575471c6b0a78f0a2fa8
Angad Narang <80539073+angadnarang@users.noreply.github.com>
2022-01-05 14:02:39 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin:['http://localhost:3000'],
+        origin:['https://optimistic-brahmagupta-f0fbb7.netlify.app'],
         credentials:true
     },
 ));
